2f37134e37f311895b41f3a2dc979d554f65d6fa
poblanopeppers <andrewy.chung@mail.utoronto.ca>
2025-09-20 18:42:06 -0400

    rename and completed final dataset

--- PV_data_analysis.R
+++ PV_data_analysis.R
@@ -58,7 +58,7 @@ pre_data <- read_csv(here("data", "raw", "PV_pre_raw-data.csv")) |>
     area_of_exploration = 13,
     affiliation = 14,
     anxiety = 15,
-    bordeom = 16,
+    boredom = 16,
     calm = 17,
     connection = 18,
     curiosity = 19,
@@ -66,7 +66,7 @@ pre_data <- read_csv(here("data", "raw", "PV_pre_raw-data.csv")) |>
     frustrations = 21,
     joy = 22,
     collaboration = 23,
-    competetition = 24,
+    competition = 24,
     colleague_wellness_support = 25,
     community = 26,
     wellness_enhance_work = 27,
@@ -78,13 +78,13 @@ pre_data <- read_csv(here("data", "raw", "PV_pre_raw-data.csv")) |>
     competition_qual = 33,
     happy = 34,
     life_interest = 35,
-    life_satisfication = 36,
+    life_satisfaction = 36,
     society_contribution = 37,
     belonging = 38,
     society_good = 39,
     people_good = 40,
     society_makes_sense = 41,
-    personality_satisfication = 42,
+    personality_satisfaction = 42,
     life_responsibility_management = 43,
     warm_trusting_relationship = 44,
     growth_opportunities = 45,
@@ -134,12 +134,12 @@ post_data <- read_csv(here("data", "raw", "PV_post_raw-data.csv")) |>
     nature_binary = 21,
     nature = 22,
     takeaway = 23,
-    post_collaboration = 24,
+    collaboration_post = 24,
     compassion_insight = 25,
     global_challenge_insight = 26,
-    collaborative_atmosphere_scientist = 27,
-    collaborative_atmosphere_attendees = 28,
-    colleague_wellness_support_post = 29,
+    retreat_fosters_collab_scientist_practitioners = 27,
+    retreat_fosters_collab_attendees = 28,
+    retreat_wellness_support_post = 29,
     community_post = 30,
     body_aware_post = 31,
     feelings_aware_post = 32,
@@ -147,13 +147,13 @@ post_data <- read_csv(here("data", "raw", "PV_post_raw-data.csv")) |>
     perception_aware_post = 34,
     happy_post = 35,
     life_interest_post = 36,
-    life_satisfication_post = 37,
+    life_satisfaction_post = 37,
     society_contribution_post = 38,
     belonging_post = 39,
     society_good_post = 40,
     people_good_post = 41,
     society_makes_sense_post = 42,
-    personality_satisfication_post = 43,
+    personality_satisfaction_post = 43,
     life_responsibility_management_post = 44,
     warm_trusting_relationship_post = 45,
     growth_opportunities_post = 46,
@@ -204,7 +204,7 @@ follow_up_data <- read_csv(here("data", "raw", "PV_follow_up_raw-data.csv")) |>
     follow_up_meditation_style_Q = 24,
     follow_up_changes_Q = 25,
     anxiety_follow_up = 26,
-    bordeom_follow_up = 27,
+    boredom_follow_up = 27,
     calm_follow_up = 28,
     connection_follow_up = 29,
     curiosity_follow_up = 30,
@@ -212,9 +212,9 @@ follow_up_data <- read_csv(here("data", "raw", "PV_follow_up_raw-data.csv")) |>
     frustrations_follow_up = 32,
     joy_follow_up = 33,
     collaboration_follow_up = 34,
-    competetition_follow_up = 35,
+    competition_follow_up = 35,
     colleague_wellness_support_follow_up = 36,
-    community_support_follow_up = 37,
+    community_follow_up = 37,
     wellness_enhance_work_follow_up = 38,
     body_aware_follow_up = 39,
     feelings_aware_follow_up = 40,
@@ -224,17 +224,17 @@ follow_up_data <- read_csv(here("data", "raw", "PV_follow_up_raw-data.csv")) |>
     competition_qual_follow_up = 44,
     happy_follow_up = 45,
     life_interest_follow_up = 46,
-    life_satisfication_follow_up = 47,
+    life_satisfaction_follow_up = 47,
     society_contribution_follow_up = 48,
     belonging_follow_up = 49,
     society_good_follow_up = 50,
     people_good_follow_up = 51,
     society_makes_sense_follow_up = 52,
-    personality_satisfication_follow_up = 53,
+    personality_satisfaction_follow_up = 53,
     life_responsibility_management_follow_up = 54,
     warm_trusting_relationship_follow_up = 55,
     growth_opportunities_follow_up = 56,
-    confidence_ideas_follow_up = 57,
+    confidence_follow_up = 57,
     life_direction_follow_up = 58,
     work_challenges_follow_up = 59,
     anything_else_follow_up = 60) |>
@@ -372,7 +372,7 @@ pre_post_data <- full_join(cleaned_pre_data, cleaned_post_data, by = common_cols
 #separating qual and quant for initial independent analysis
 qual_pre_post <- pre_post_data |>
   select(-c(anxiety:life_direction,
-            collaborative_atmosphere_scientist:life_direction_post))
+            retreat_fosters_collab_scientist_practitioners:life_direction_post))
 
 quant_pre_post <- pre_post_data |>
   select(-c(collaboration_qual: anything_else,
@@ -380,8 +380,8 @@ quant_pre_post <- pre_post_data |>
             work_challenges_post:anything_else_post))
 
 # Writing data
-write_csv(pre_post_data, here("data", "processed", "pre_post_data.csv"))
-write_csv(qual_pre_post, here("data", "processed", "qual_pre_post_data.csv"))
+# write_csv(pre_post_data, here("data", "processed", "pre_post_data.csv"))
+# write_csv(qual_pre_post, here("data", "processed", "qual_pre_post_data.csv"))
 
 # MATCHING POST FOLLOW UP Participants  -----------------------------------------------------------------------------------------------------------------
 
@@ -436,12 +436,11 @@ new_participants_follow <- follow_up_data |>
   select(-c(last_id_num, presurvey_post_check)) |>
   relocate(id, .before= time_point)
 
-# NEW UPDATED MATCHING LOG
-all_data_follow <- bind_rows(exact_matches_follow, matched_follow
-                             , new_participants_follow) |>
-  relocate(follow_id, .after = cleaned_id)
+#merge pre and pre post participants
+follow_up <- bind_rows(exact_matches_follow, matched_follow, new_participants_follow)
 
-merged_follow <- all_data_follow |>
+#group to remove redundancies
+merged_follow <- follow_up |>
   #group to remove outdated duplicates
   group_by(id) |>
   slice_tail(n=1) |>
@@ -450,69 +449,83 @@ merged_follow <- all_data_follow |>
   arrange(id_num) |>
   select(-id_num)
 
+# NEW UPDATED MATCHING LOG
 final_matching_log <- merged_follow |>
-  select(id:presurvey_check)
+  select(id:time_point)
 
 write_csv(final_matching_log, here("data", "processed",
-                                      "pre_post_follow_matching_log.csv"))
-# # MERGING Pre & Post Data
-# 
-# # Find intersecting column names automatically
-# # Create new matching log
-# 
-# # first cleaning pre and post
-# cleaned_post_data <- bind_rows(exact_matches, weird_matches, new_participants) |>
-#   relocate(post_id, .after = id) |>
-#   select(-c(post_id, name, cleaned_id)) |>
-#   relocate(age:nature, .after = time_point) |>
-#   distinct(id, .keep_all = TRUE)
-# 
-# cleaned_pre_data <- matching_log |>
-#   left_join(pre_data |> select(-name), by = "cleaned_id") |>
-#   mutate(
-#     time_point = ifelse(
-#       !is.na(time_point.x) & !is.na(time_point.y),
-#       paste(time_point.x),
-#       coalesce(time_point.x, time_point.y)
-#     )
-#   ) |>
-#   select(-c(time_point.x, time_point.y, name, cleaned_id, matching_id)) |>
-#   relocate(time_point, .after = id) |>
-#   distinct(id, .keep_all = TRUE)
-# 
-# # MERGED PRE POST DATA 
-# 
-# common_cols <- intersect(names(cleaned_pre_data), names(cleaned_post_data))
-# pre_post_data <- full_join(cleaned_pre_data, cleaned_post_data, by = common_cols)
-#   #person did not enter name and has not consented
-# 
-# # removes duplicates and merges them together while also fixing pre post
-# pre_post_data <- pre_post_data |>
-#   group_by(id) |>
-#   summarize(
-#     time_point = paste(
-#       unique(unlist(strsplit(time_point, ", "))) |> 
-#         factor(levels = c("pre", "post")) |> 
-#         sort() |> 
-#         as.character(),
-#       collapse = ", "
-#     ),
-#     across(everything(), ~ coalesce(.[!is.na(.)][1])),  # Take first non-NA value
-#     .groups = "drop"
-#   ) |>
-#   slice(-c(11)) 
-# 
-# #separating qual and quant for initial independent analysis
-# qual_pre_post <- pre_post_data |>
-#   select(-c(anxiety:life_direction, 
-#             collaborative_atmosphere_scientist:life_direction_post))
-# 
-# quant_pre_post <- pre_post_data |> 
-#   select(-c(collaboration_qual: anything_else, 
-#             takeaway:global_challenge_insight, 
-#             work_challenges_post:anything_else_post))
+                                      "final_matching_log.csv"))
+# MERGING Pre & Post Data
+
+# Find intersecting column names automatically
+# Create new matching log
+
+# first cleaning pre and post
+
+cleaned_follow_data <- merged_follow |>
+  select(-c(name, cleaned_id)) |>
+  relocate(age:nature, .after = time_point)
+
+# MERGED PRE POST DATA
+
+cols <- intersect(names(pre_post_data), names(cleaned_follow_data))
+final_data <- full_join(pre_post_data, cleaned_follow_data, by = cols) |>
+  group_by(id) |>
+  summarize(
+    across(age:anything_else_post, 
+           ~ if_else(n() >= 2, first(.x), first(.x))),
+    across(c(time_point, follow_up_meditation_binary:anything_else_follow_up), 
+           ~ if_else(n() >= 2, nth(.x, 2), first(.x)))
+  )|>
+  ungroup() |>
+  mutate(id_num = parse_number(id)) |>
+  arrange(id_num) |>
+  select(-c(id_num, presurvey_check)) |>
+  relocate(c(retreat_fosters_collab_scientist_practitioners:retreat_wellness_support_post,
+             collaboration_follow_up: competition_qual_follow_up), 
+           .after = wellness_enhance_work) |>
+  relocate(c(anxiety, anxiety_follow_up, boredom, boredom_follow_up, calm,
+             calm_follow_up, connection, connection_follow_up, curiosity,
+             curiosity_follow_up, despair, despair_follow_up, frustrations, 
+             frustrations_follow_up, joy, joy_follow_up, collaboration, 
+             collaboration_follow_up,
+             competition, competition_follow_up, colleague_wellness_support, 
+             colleague_wellness_support_follow_up, community, community_post, 
+             community_follow_up,
+             wellness_enhance_work, wellness_enhance_work_follow_up, 
+             retreat_fosters_collab_scientist_practitioners:retreat_wellness_support_post,
+             body_aware, body_aware_post, body_aware_follow_up, feelings_aware,
+             feelings_aware_post, feelings_aware_follow_up,  mind_aware, mind_aware_post,
+             mind_aware_follow_up, perception_aware, perception_aware_post, 
+             perception_aware_follow_up, happy, happy_post, happy_follow_up, 
+             life_interest, life_interest_post, life_interest_follow_up, 
+             life_satisfaction, life_satisfaction_post, life_satisfaction_follow_up,
+             society_contribution, society_contribution_post, society_contribution_follow_up,
+             belonging, belonging_post, belonging_follow_up, society_good, society_good_post,
+             society_good_follow_up,people_good, people_good_post, 
+             people_good_follow_up, society_makes_sense, society_makes_sense_post,
+             society_makes_sense_follow_up, personality_satisfaction, 
+             personality_satisfaction_post, personality_satisfaction_follow_up,
+             life_responsibility_management, life_responsibility_management_post, 
+             life_responsibility_management_follow_up,
+             warm_trusting_relationship, warm_trusting_relationship_post, 
+             warm_trusting_relationship_follow_up, growth_opportunities,
+             growth_opportunities_post, growth_opportunities_follow_up, confidence_ideas,
+             confidence_ideas_post, confidence_follow_up, life_direction, 
+             life_direction_post, life_direction_follow_up), .after= plum_village_sangha) |>
+  relocate(c(collaboration_post,collaboration_qual_follow_up, 
+             competition_qual_follow_up), .after= competition_qual) |>
+  relocate(c(work_challenges_post, work_challenges_follow_up), .after = work_challenges) |>
+  relocate(c(anything_else, anything_else_post, anything_else_follow_up), .after = follow_up_changes_Q) |>
+  relocate(time_point, .after = id)
+
+#separating qual and quant for initial independent analysis
+qual <- final_data |>
+  select(-c(anxiety:life_direction_follow_up))
+
+quant <- final_data |>
+  select(-c(collaboration_qual: anything_else_follow_up))
 
 # Writing data
-# write_csv(pre_post_data, here("data", "processed", "pre_post_data.csv"))
-# write_csv(qual_pre_post, here("data", "processed", "qual_pre_post_data.csv"))
-# write_csv(quant_pre_post, here("data", "processed", "quant_pre_post_data.csv"))
+write_csv(qual, here("data", "processed", "qual_data.csv"))
+write_csv(quant, here("data", "processed", "quant_data.csv"))
